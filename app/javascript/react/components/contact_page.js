737e8c964b57c1a7c9951b8269285c367dee6179
Michael Decroce-Movson <mdecrocemovson@gmail.com>
2020-02-25 16:49:03 -0500

    added mailer

--- app/javascript/react/components/contact_page.js
+++ app/javascript/react/components/contact_page.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 const ContactPage = () => {
   const [firstName, setFirstName] = useState('');
@@ -7,18 +8,29 @@ const ContactPage = () => {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs.sendForm('gmail', 'template_UDbNQqJd', e.target, 'user_N3vEIPlISgo5zdl7eDmoh')
+    .then ((result) => {
+      debugger
+    }), (error) => {
+      debugger
+    }
+  }
+
   return (
     <div className="ContactPage">
       <div className="ContactPage-intro">
         <h3>Lets start a conversation!</h3>
       </div>
-      <form>
+      <form onSubmit={sendEmail}>
         <div className="ContactPage-form">
           <div className="ContactPage-firstInputs">
             <input
               placeholder="First Name"
               className="ContactPage-input"
               value={firstName}
+              name="first_name"
               onChange={e => setFirstName(e.target.value)}
               type="text"
             />
@@ -26,12 +38,14 @@ const ContactPage = () => {
               placeholder="Last Name"
               className="ContactPage-input"
               value={lastName}
+              name="last_name"
               onChange={e => setLastName(e.target.value)}
               type="text"
             />
             <input
               placeholder="Email"
               value={email}
+              name="email"
               onChange={e => setEmail(e.target.value)}
               type="text"
             />
@@ -40,6 +54,7 @@ const ContactPage = () => {
             <textarea
               placeholder="Message"
               value={message}
+              name="message"
               onChange={e => setMessage(e.target.value)}
             />
           </div>
